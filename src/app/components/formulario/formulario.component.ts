9c5940e14d20d040466fd9c940a0dd49f0d22f6d
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-02-26 12:36:13 +0100

    Se aplican estilos a los textos de validación y a los inputs en función de su estado. Se añaden diferentes validaciones a los distintos campos. Se crea una validación de formulario para comprobar la coincidencia de los passwords

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -13,27 +13,35 @@ export class FormularioComponent {
   constructor() {
     this.formModel = new FormGroup({
       first_name: new FormControl("",[
-        Validators.required
+        Validators.required,
+        Validators.minLength(3)
       ]),
       last_name: new FormControl("",[
         Validators.required
       ]),
       username: new FormControl("",[
-        Validators.required
+        Validators.required,
+        Validators.minLength(3)
       ]),
       email: new FormControl("",[
-        Validators.required
+        Validators.required,
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
       ]),
       image: new FormControl("",[
-        Validators.required
+        Validators.required,
+        Validators.pattern(/((ht|f)tp(s?))(:((\/\/)(?!\/)))(((w){3}\.)?)([a-zA-Z0-9\-_]+(\.(com|edu|gov|int|mil|net|org|biz|info|name|pro|museum|es|dev|co\.uk)))(\/(?!\/))(([a-zA-Z0-9\-_\/]*)?)([a-zA-Z0-9])+\.((jpg|jpeg|gif|png)(?!(\w|\W)))/)
+        //Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')
       ]),
       password: new FormControl("",[
-        Validators.required
+        Validators.required,
+        Validators.pattern(/^(?=(.*[a-zA-Z].*){2,})(?=.*\d.*)(?=.*\W.*)[a-zA-Z0-9\S]{8,15}$/)
       ]),
       repetirpassword: new FormControl("",[
         Validators.required
       ])
-    },[]);
+    },[
+      this.checkPassword
+    ]);
 
   }
 
@@ -46,5 +54,16 @@ export class FormularioComponent {
       return true;
     }
     return false;
+  }
+
+  checkPassword(pFormValue: AbstractControl) {
+
+    const password: string = pFormValue.get('password')?.value;
+    const repitePassword: string = pFormValue.get('repetirpassword')?.value;
+
+    if(password !== repitePassword) {
+      return { 'checkpassword': true }
+    }
+    return null;
   }
 }
