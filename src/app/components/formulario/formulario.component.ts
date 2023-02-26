d83b6cb6d16b8c30a9372326f541707620774384
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-02-26 09:10:02 +0100

    Se traslada el formulario y su lógica del componente nuevo-usuario al componente formulario

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  formModel: FormGroup;
+
+  constructor() {
+    this.formModel = new FormGroup({
+      first_name: new FormControl("",[
+        Validators.required
+      ]),
+      last_name: new FormControl("",[
+        Validators.required
+      ]),
+      username: new FormControl("",[
+        Validators.required
+      ]),
+      email: new FormControl("",[
+        Validators.required
+      ]),
+      image: new FormControl("",[
+        Validators.required
+      ]),
+      password: new FormControl("",[
+        Validators.required
+      ]),
+      repetirpassword: new FormControl("",[
+        Validators.required
+      ])
+    },[]);
+
+  }
+
+  getDataForm() {
+    console.log(this.formModel.value);
+  }
 }
--- src/app/components/nuevo-usuario/nuevo-usuario.component.ts
+++ src/app/components/nuevo-usuario/nuevo-usuario.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-usuario',
@@ -8,22 +7,5 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class NuevoUsuarioComponent {
 
-  formModel: FormGroup;
 
-  constructor() {
-    this.formModel = new FormGroup({
-      first_name: new FormControl("",[]),
-      last_name: new FormControl("",[]),
-      username: new FormControl("",[]),
-      email: new FormControl("",[]),
-      image: new FormControl("",[]),
-      password: new FormControl("",[]),
-      repetirpassword: new FormControl("",[])
-    },[]);
-
-  }
-
-  getDataForm() {
-    console.log(this.formModel.value);
-  }
 }
